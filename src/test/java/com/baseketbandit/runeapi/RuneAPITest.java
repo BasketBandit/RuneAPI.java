3fee33f92a7d26d62c35e68b3ee51429e00f0c2a
BasketBandit <errorsfound@hotmail.co.jp>
2019-10-16 12:34:07 +0100

    updated testing

--- src/test/java/com/baseketbandit/runeapi/RuneAPITest.java
+++ src/test/java/com/baseketbandit/runeapi/RuneAPITest.java
@@ -2,34 +2,34 @@ package com.baseketbandit.runeapi;
 
 import com.baseketbandit.runeapi.entity.Skill;
 import com.baseketbandit.runeapi.entity.Type;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-public class RuneAPITest {
+class RuneAPITest {
 
     @Test
-    public void getStats() {
+    void getStats() {
         Map<Type, Skill> stats = RuneAPI.getStats("xwr");
         assert(!stats.isEmpty());
         assert(stats.size() == 24);
     }
 
     @Test
-    public void getStatsNotFound() {
+    void getStatsNotFound() {
         Map<Type, Skill> stats = RuneAPI.getStats("abcdefghijklmnopqrstuvwxyz");
         assert(stats.isEmpty());
     }
 
     @Test
-    public void getStatsNameSpace() {
+    void getStatsNameSpace() {
         Map<Type, Skill> stats = RuneAPI.getStats("Iron Lewiso");
         assert(!stats.isEmpty());
         assert(stats.size() == 24);
     }
 
     @Test
-    public void getStatsMapped() {
+    void getStatsMapped() {
         Map<Type, Skill> stats = RuneAPI.getStats("xwr");
         assert(!stats.isEmpty());
         assert(stats.size() == 24);
