73f8be51ce9180e8e83f4080d1ab3675cac0130a
BasketBandit <errorsfound@hotmail.co.jp>
2020-11-12 03:00:50 +0000

    change source compatibility from java 11 -> java 8

--- src/main/java/com/baseketbandit/runeapi/io/RequestHandler.java
+++ src/main/java/com/baseketbandit/runeapi/io/RequestHandler.java
@@ -8,8 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
